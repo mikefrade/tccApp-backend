60636ed6b2c6222096a9cb85553a4b7a290a3b72
Mike Frade <mikefrade2704@gmail.com>
2018-05-26 20:56:22 -0300

    Projeto Criado

---
+++ src/main/java/com/mikefrade/tccapp/TccappApplication.java
@@ -0,0 +1,12 @@
+package com.mikefrade.tccapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccappApplication.class, args);
+	}
+}
